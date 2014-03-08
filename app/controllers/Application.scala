ae8368a8feaa80a4d72d1c24ecdc0186b7993a78
danysantiago <daniel.santiago5@upr.edu>
2014-03-07 21:19:57 -0400

    Added script execution and branch checking

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,19 +1,23 @@
 package controllers
 
-import play.Logger
 import play.api._
 import play.api.mvc._
+import scala.sys.process._
 
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok("Scala-Hook up and listening!")
   }
 
-  def git = Action { request =>
-    val body = request.body
-    println(body)
-    Ok("Cool")
+  def git = Action(parse.json) { request =>
+    (request.body \ "ref").asOpt[String].map { ref =>
+      if (ref.equals("refs/heads/master"))
+        println("sh script.sh"!!)
+      Ok("All Good!")
+    }.getOrElse {
+      BadRequest("Bad JSON Payload, is this a Git Hook?")
+    }
   }
 
 }
---
+++ script.sh
@@ -0,0 +1,6 @@
+ #!/bin/bash
+
+echo "Update Code"
+echo "====================================="
+# cd ../myPlayApp
+# git pull
